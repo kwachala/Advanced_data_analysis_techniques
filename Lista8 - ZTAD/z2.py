881d959f4fe9888b749c9001dd7781771fe94710
kwachala <dragonik@onet.pl>
2023-05-16 14:48:44 +0200

    Simple changes to data in 8th list

--- Lista8 - ZTAD/z2.py
+++ Lista8 - ZTAD/z2.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_excel('254271.xls', sheet_name='Dane')
 
-to_drop = df[(df['status pozyczki'] == 'odmowa') | (df['kwota otrzymana'] > 900)]
+to_drop = df[(df['status pozyczki'] == 'odmowa') | (df['kwota kredytu'] > 900)]
 df.drop(to_drop.index, inplace=True)
 df.drop('status pozyczki', axis=1, inplace=True)
 print(df.head(10))
